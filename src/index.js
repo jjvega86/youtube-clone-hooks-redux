acc07310246d2435652f06af8d13bd6702cc9f34
JJ <jj.vega86@gmail.com>
2021-12-17 17:49:24 -0500

    Added Redux store and provider to project

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import { render } from "react-dom";
 import App from "./App.js";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
-render(<App />, document.getElementById("root"));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
